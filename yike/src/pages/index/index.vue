daf557e38e6c52f0e21f95e6f7a6a60fe4c21668
邓威 <1536698087@qq.com>
2022-08-21 18:13:40 +0800

    yike：首页头部

--- yike/src/pages/index/index.vue
+++ yike/src/pages/index/index.vue
@@ -1,8 +1,16 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+    <view class="header">
+      <view class="top-bar">
+        <image class="title" src="../../static/index/logo.png" mode="aspectFit" lazy-load="false" />
+        <image class="action" src="../../static/index/add.png" mode="aspectFit" lazy-load="false" />
+      </view>
+      <view class="search-box">
+        <view class="input">
+          <image class="icon" src="../../static/index/search.png" mode="aspectFit" lazy-load="false" />
+          <view>搜索</view>
+        </view>
+      </view>
     </view>
   </view>
 </template>
@@ -19,30 +27,45 @@ export default {
 }
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
+<style lang="scss">
+  .header {
+    border-bottom: 1rpx solid $uni-border-color;
+    .top-bar {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: relative;
+      height: 88rpx;
+      background-color: $uni-bg-color;
+      .title {
+        width: 88rpx;
+        height: 42rpx;
+      }
+      .action {
+        position: absolute;
+        top: 50%;
+        right: 32rpx;
+        transform: translateY(-50%);
+        width: 48rpx;
+        height: 48rpx;
+      }
+    }
+    .search-box {
+      padding: 10rpx;
+      font-size: 28rpx;
+      .input {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 60rpx;
+        border-radius: $uni-border-radius-base;
+        background-color: $uni-bg-color-grey;
+        color: $uni-text-color-placeholder;
+        .icon {
+          width: 48rpx;
+          height: 48rpx;
+        }
+      }
+    }
+  }
 </style>
