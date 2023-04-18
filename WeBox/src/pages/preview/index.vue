4edd420973f197f74682038dd512736e6a0bb82e
邓威 <1536698087@qq.com>
2023-04-18 22:28:18 +0800

    webox | 添加预览页

---
+++ WeBox/src/pages/preview/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <web-view :src="h5Link" />
+</template>
+<script setup lang="ts">
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+const h5Link = ref('')
+onLoad(({ link }: any) => {
+  h5Link.value = link
+})
+</script>
