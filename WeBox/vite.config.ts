fde27eb4e498b7dc30ef7d66643f9de0fd620221
dw <1536698087@qq.com>
2023-04-25 13:32:34 +0800

    webox | 配置代理

--- WeBox/vite.config.ts
+++ WeBox/vite.config.ts
@@ -4,4 +4,15 @@ import uni from "@dcloudio/vite-plugin-uni";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
-});
+  server: {
+    open: true,
+    proxy: {
+      // 带选项写法：http://localhost:5173/api/bar -> http://jsonplaceholder.typicode.com/bar
+      '/h5api': {
+        target: 'https://dengcaocao.github.io',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/h5api/, ''),
+      }
+    }
+  }
+})
