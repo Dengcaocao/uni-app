bf8298ffa329fe89dbb112fd1b3ef883f7630085
邓威 <1536698087@qq.com>
2023-05-21 17:06:44 +0800

    webox | 修改首页样式、布局

--- WeBox/src/pages/index/index.vue
+++ WeBox/src/pages/index/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <view class="uni-margin-wrap">
+  <view class="product-container">
+    <view class="product-info">
+      <view class="product-title">WeBox</view>
+      <view class="product-desc">小样展示平台</view>
+    </view>
+    <view class="product-icon">
+      <view>we</view>
+      <view>box</view>
+    </view>
+  </view>
+  <view class="content">
     <swiper
       class="swiper"
       circular
       :indicator-dots="true"
-      indicator-active-color="#B0D084"
+      indicator-active-color="#FFFFFF"
       :autoplay="true"
       :interval="2000"
       :duration="500">
@@ -12,35 +22,32 @@
         <image class="banner-image" :src="item.pic" />
       </swiper-item>
     </swiper>
-  </view>
 
-  <view class="firend-chain">
-    <view
-      class="item"
-      v-for="item in firendChain"
-      :key="item.uuid"
-      @click="pageHome(item.link)"
-    >
-      <image :src="item.avatar" />
-      <view class="nickname single-line-text">{{item.nickName}}</view>
-    </view>
-  </view>
-
-  <view class="content-box">
-    <view class="content-group" v-for="item in catetory" :key="item.type">
-      <view class="header">
-        <view class="title">{{item.type}}</view>
-        <view class="more">更多</view>
+    <view class="firend-chain">
+      <view
+        class="item"
+        v-for="item in firendChain"
+        :key="item.uuid"
+        @click="pageHome(item.link)"
+      >
+        <image :src="item.avatar" />
+        <view class="nickname single-line-text">{{item.nickName}}</view>
       </view>
-      <view class="content">
-        <view class="demo shadow"
-          v-for="demo in item.data"
-          :key="demo.link"
-          @click="preview(demo.link)">
-          <image class="demo-img-small" :src="demo.samllImage" />
-          <view class="demo-info">
-            <view class="demo-title single-line-text">{{demo.title}}</view>
-            <view class="demo-description single-line-text">{{demo.description}}</view>
+    </view>
+
+    <view class="up-to-date">
+      <view class="head">最新</view>
+      <view class="demo-list">
+        <view
+          class="demo-item"
+          v-for="item in catetory"
+          :key="item.uuid"
+        >
+          <image class="preview-img" :src="item.pic" />
+          <view class="info">
+            <view class="title">{{item.title}}</view>
+            <view class="description">{{item.description}}</view>
+            <view class="date">3天前</view>
           </view>
         </view>
       </view>
@@ -73,11 +80,15 @@ const getFirendChain = async () => {
   }
 }
 
-let catetory: catetoryType[] = reactive([])
+let catetory: any[] = reactive([])
 const getCatetory = async () => {
   const res: any = await Api.queryCatetory()
+  const upToDateData = res.data.data
+    .map((item: any) => item.data)
+    .map((item: any) => item.slice(0, 5)) // 取前5
+    .flat()
   // 处理直接赋值视图不更新的问题
-  Object.assign(catetory, res.data.data)
+  Object.assign(catetory, upToDateData)
 }
 
 const pageHome = (url: string) => {
@@ -97,20 +108,56 @@ onMounted(async () => {
 })
 </script>
 <style scoped lang="scss">
+.product-container {
+  display: flex;
+  justify-content: space-between;
+  height: 300rpx;
+  padding: 60rpx $uni-spacing-row-lg $uni-spacing-row-lg $uni-spacing-row-lg;
+  background-image: linear-gradient($uni-bg-color 50%, #FFFFFF);
+  .product-info {
+    .product-title {
+      font-size: 54rpx;
+      font-weight: bolder;
+      letter-spacing: 4rpx;
+    }
+    .product-desc {
+      letter-spacing: 12rpx;
+      font-size: $uni-font-size-sm;
+    }
+  }
+  .product-icon {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 80rpx;
+    height: 80rpx;
+    color: #fff;
+    margin-right: 60rpx;
+    font-size: $uni-font-size-base;
+    font-weight: 600;
+    line-height: $uni-font-size-base;
+    border-radius: $uni-border-radius-sm;
+    background-color: #00b96b;
+  }
+}
+.content {
+  margin-top: -180rpx;
+}
+
 .swiper {
   height: 300rpx;
+  padding: $uni-spacing-row-lg;
+  border-radius: $uni-border-radius-lg;
   .banner-image,
   .banner-image img {
     width: 100%;
     height: 100%;
   }
 }
-.swiper-item {
-  display: block;
-  height: 300rpx;
-  line-height: 300rpx;
-  text-align: center;
-}
+// .swiper .uni-swiper-wrapper {
+//   border-radius: $uni-border-radius-lg;
+// }
 .firend-chain {
   display: grid;
   grid-template-columns: repeat(4, 120rpx);
@@ -124,67 +171,50 @@ onMounted(async () => {
     image {
       width: 90rpx;
       height: 90rpx;
+      background-color: $uni-bg-color;
       border-radius: $uni-border-radius-circle;
     }
     .nickname {
-      font-size: 24rpx;
+      font-size: $uni-font-size-sm;
+      color: $uni-text-color;
     }
   }
 }
-.content-box {
-  overflow: hidden;
-  background-color: #f6f7fa;
-  .content-group {
-    margin-top: $uni-spacing-col-lg;
-    padding: $uni-spacing-row-base;
-    background-color: #fff;
-    .header {
+.up-to-date {
+  padding: $uni-spacing-row-lg;
+  .head {
+    font-size: $uni-font-size-base;
+    font-weight: 600;
+    color: $uni-text-color;
+    letter-spacing: 4rpx;
+  }
+  .demo-item {
+    display: flex;
+    // align-items: center;
+    margin-top: 32rpx;
+    .preview-img {
+      width: 180rpx;
+      height: 180rpx;
+      border-radius: $uni-border-radius-base;
+      background-color: $uni-bg-color;
+    }
+    .info {
       display: flex;
-      justify-content: space-between;
-      align-items: center;
+      flex-direction: column;
+      justify-content: space-evenly;
+      font-size: $uni-font-size-base;
+      margin-left: $uni-spacing-row-lg;
       .title {
-        font-size: 32rpx;
+        font-size: $uni-font-size-lg;
+        color: $uni-text-color;
+        font-weight: 600;
+        // padding-bottom: $uni-spacing-col-sm;
       }
-      .more {
-        font-size: 24rpx;
+      .description {
         color: $uni-text-color-grey;
       }
-    }
-    .content {
-      display: flex;
-      justify-content: space-between;
-      flex-wrap: wrap;
-      margin-top: $uni-spacing-col-base;
-      .demo {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 340rpx;
-        height: 120rpx;
-        padding: $uni-spacing-row-base;
-        box-sizing: border-box;
-        border-radius: 8rpx;
-        margin-bottom: 16rpx;
-        background-color: $uni-bg-color-hover;
-        .demo-img-small {
-          width: 98rpx;
-          height: 100%;
-          margin-right: $uni-spacing-row-base;
-          flex-shrink: 0;
-        }
-        .demo-info {
-          flex: 1;
-          .demo-title {
-            width: 100rpx;
-            font-size: $uni-font-size-base;
-            color: #fff;
-          }
-          .demo-description {
-            font-size: $uni-font-size-sm;
-            color: $uni-text-color-placeholder;
-            line-height: 32rpx;
-          }
-        }
+      .date {
+        color: $uni-text-color-placeholder;
       }
     }
   }
