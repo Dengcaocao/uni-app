9c2b66043d55753196f85c2b96b36fb695fd3d6a
dw <1536698087@qq.com>
2023-05-22 16:31:00 +0800

    webox | 添加全局样式

--- WeBox/src/App.vue
+++ WeBox/src/App.vue
@@ -4,15 +4,26 @@
 <style lang="scss">
 page {
   color: $uni-text-color;
+  font-size: $uni-font-size-base;
 }
 .single-line-text {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
 }
-.shadow {
-  box-shadow: 0px 6px 16px -8px rgba(0, 0, 0, 0.08),
-              0px 9px 28px 0px rgba(0, 0, 0, 0.05),
-              0px 12px 48px 16px rgba(0, 0, 0, 0.03);
+.shadow1 {
+  box-shadow: 0px 2rpx 4rpx -4rpx rgba(0, 0, 0, 0.16),
+              0px 4rpx 12rpx 0px rgba(0, 0, 0, 0.12),
+              0px 10rpx 24rpx 8rpx rgba(0, 0, 0, 0.9);
+}
+.shadow2 {
+  box-shadow: 0px 6rpx 12rpx -8rpx rgba(0, 0, 0, 0.08),
+              0px 12rpx 32rpx 0px rgba(0, 0, 0, 0.05),
+              0px 18rpx 56rpx 16rpx rgba(0, 0, 0, 0.03);
+}
+.shadow3 {
+  box-shadow: 0px 12rpx 32rpx -16rpx rgba(0, 0, 0, 0.08),
+              0px 18rpx 56rpx 0px rgba(0, 0, 0, 0.05),
+              0px 24rpx 96rpx 32rpx rgba(0, 0, 0, 0.03);
 }
 </style>
