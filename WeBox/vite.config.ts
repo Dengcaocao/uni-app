50ee0dd6283ecd02b715bfa53debc4c192c825a0
dw <1536698087@qq.com>
2023-05-23 11:00:19 +0800

    webox | 配置基础路径

--- WeBox/vite.config.ts
+++ WeBox/vite.config.ts
@@ -3,6 +3,7 @@ import uni from "@dcloudio/vite-plugin-uni";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mProjects/webox/',
   plugins: [uni()],
   server: {
     open: true,
